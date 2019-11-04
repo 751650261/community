f9567142369dc64b8821fc64ae92d496708201ac
751650261 <751650261@qq.com>
2019-11-04 10:44:51 +0800

    extract github config from code to properties

--- src/main/java/com/cwq/springbootcommunity/controller/AuthorizeController.java
+++ src/main/java/com/cwq/springbootcommunity/controller/AuthorizeController.java
@@ -4,6 +4,7 @@ import com.cwq.springbootcommunity.dto.AccessTokenDTO;
 import com.cwq.springbootcommunity.dto.GitHubUser;
 import com.cwq.springbootcommunity.provider.GitHubProvider;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -18,15 +19,24 @@ public class AuthorizeController {
     @Autowired
     private GitHubProvider gitHubProvider;
 
+    @Value("{$github.client.id}")
+    private String clientId;
+
+    @Value("{$github.client.secret}")
+    private String clientSercet;
+
+    @Value("{$github.redirect.uri}")
+    private String redirectUri;
+
     @GetMapping("/callback")
     public String callback(@RequestParam(name="code") String code,
                            @RequestParam(name = "state") String state){
         AccessTokenDTO accessTokenDTO = new AccessTokenDTO();
         accessTokenDTO.setCode(code);
-        accessTokenDTO.setRedirect_uri("http://localhost:8080/callback");
+        accessTokenDTO.setRedirect_uri(redirectUri);
+        accessTokenDTO.setClient_id(clientId);
+        accessTokenDTO.setClient_secret(clientSercet);
         accessTokenDTO.setState(state);
-        accessTokenDTO.setClient_id("a663ad600416ee18f10e");
-        accessTokenDTO.setClient_secret("ca2aa7122adc082e6dc199717e4a366d75968578");
         String accessToken = gitHubProvider.getAccessToken(accessTokenDTO);
         GitHubUser user = gitHubProvider.getUser(accessToken);
         System.out.println(user.getName());
