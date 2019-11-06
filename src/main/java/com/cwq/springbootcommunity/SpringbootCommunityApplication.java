be36f285039a4f96bc82525da30254252271a564
751650261 <751650261@qq.com>
2019-11-06 22:56:18 +0800

    update

--- src/main/java/com/cwq/springbootcommunity/SpringbootCommunityApplication.java
+++ src/main/java/com/cwq/springbootcommunity/SpringbootCommunityApplication.java
@@ -1,9 +1,11 @@
 package com.cwq.springbootcommunity;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@MapperScan(basePackages = "com.cwq.springbootcommunity.mapper")
 public class SpringbootCommunityApplication {
 
     public static void main(String[] args) {
